02901f227eec370bb37b7dd78f3c9d91ae168191
MauriiceBTW <141180070+MauriiceBTW@users.noreply.github.com>
2023-08-05 21:48:44 +0200

    Update HealPlugin.java

--- HealPlugin.java
+++ HealPlugin.java
@@ -1,1 +1,21 @@
+package me.maurice.healplugin;
+// change this to your package if you copy & paste!
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class HealPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+        // Register /heal command
+        getCommand("heal").setExecutor(new HealCommand());
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
 
